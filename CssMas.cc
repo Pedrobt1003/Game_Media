cfe5f962afa986fc643cf1e05bb6b3f03a380e80
Pedrobt1003 <65671778+Pedrobt1003@users.noreply.github.com>
2020-05-20 18:39:43 +0200

    Create CssMas.cc

---
+++ CssMas.cc
@@ -0,0 +1,97 @@
+@charset "utf-8";
+.CssMas {
+}
+.Ejercicio {
+	margin: 0px;
+	padding: 0px;
+}
+.contenedor{
+	width: 100%;
+	margin: 0px auto; 
+	max-width: 1500px;
+}
+
+.cabezera{
+	padding-top:2px;
+    width: 100%;
+    height: 150px;
+    text-align: center;
+	
+
+}
+.Menu{
+	float-aling: center;
+	width: 100%;
+	height: 80px;
+	
+}
+.Menu2{
+	width: 100%;
+	height: 80px;
+	text-align: center;
+	
+	
+	
+}
+.parte1{
+	width:10%;
+	float : left;
+	
+}
+.parte2{
+	width:80%;
+	float : left;
+	background-color: rgba(255,247,0,1.00); opacity: 0.8;
+	
+}
+.contenedor2{
+	width: 100%;
+	margin: 0px auto; 
+	max-width: 15000px;
+	background-color: rgba(255,255,255,1.00); opacity: 0.9;
+	height: 700px;
+	
+}
+#boton1{
+    width: 25%;
+    height: 45px;
+    float : left;
+    text-align: center;
+	 
+    
+	
+	
+}
+#boton2{
+    width: 25%;
+    height: 45px;
+   
+    float : left;
+    text-align: center;
+	
+}
+#boton3{
+    width: 25%;
+    height: 45px;
+    float : left;
+    text-align: center;
+	
+}
+#boton4{
+    width: 25%;
+    height: 45px;
+    float : left;
+    text-align: center;
+	
+}
+#cuerpo{
+	width: 100%;
+	height: 400px;
+	
+}
+#pie{
+	width: 100%;
+	height: 50px;
+	background-color: rgba(255,255,255,1.00); opacity: 0.5;
+
+}
